bcd52e1360cee028c1e367e7984dd6d7b42a1a88
“AntonioKolosov” <anton.kkolosov@gmail.com>
2024-08-01 12:51:09 +0000

    extend utility

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 python main.py path/to/input/file path/to/output/file
 """
 
+from itertools import groupby
 import re
 import json
 import argparse
@@ -9,45 +10,47 @@ import os
 
 def read_subtitle_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
-
-def split_into_chunks(subtitle_text):
-    chunks = re.split(r'\d+\.\s*\n', subtitle_text)
-    return [chunk.strip() for chunk in chunks if chunk.strip()]
-
-def parse_chunk(chunk):
-    lines = chunk.split('\n')
-    if len(lines) < 2:
-        return None
-
-    content = lines[:2]
-
-    return '\n'.join(content)
-
-def convert_to_custom_format(chunks):
-    custom_format = []
-
-    for chunk in chunks:
-        chunk_value = parse_chunk(chunk)
-        if chunk_value:
-            custom_format.append({"chank": chunk_value})
-
-    return custom_format
+        return [line.rstrip() for line in file]
+        # return file.readlines()
 
 def save_to_json(data, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
+def divide_to_chunks(subtile_text):
+    """"""
+    chunks = []
+    chunk_value = ''
+    for line in subtile_text:
+        if line.strip():
+            if re.match(r"\d", line) is not None:
+                if chunk_value != '':
+                    n = sum(c.isdigit() for c in line)
+                    s = line[n]
+                    if s != '.':
+                        # If it is not new chunks id
+                        chunk_value += line + '\n'
+                    chunk = {"chank": chunk_value}
+                    chunks.append(chunk)
+                    chunk_value = ''
+                continue
+            else:
+                chunk_value += line + '\n'
+    # last chunk
+    if chunk_value != '':
+        chunk = {"chank": chunk_value}
+        chunks.append(chunk)
+    
+    return chunks
+
 def main(input_file, output_file):
     subtitle_text = read_subtitle_file(input_file)
-    
-    chunks = split_into_chunks(subtitle_text)
-    
-    parsed_chunks = [parse_chunk(chunk) for chunk in chunks if parse_chunk(chunk)]
-    
-    custom_format = convert_to_custom_format(parsed_chunks)
-    
-    save_to_json(custom_format, output_file)
+    chunks = divide_to_chunks(subtitle_text)
+    content = {
+        "name": "ru_data", # !!!!!! from file name
+        "content": chunks
+    }
+    save_to_json(content, output_file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Subtitle chunk converter utility.')
